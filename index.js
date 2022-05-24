06c1c011e4a2a5045800d99295a854c0ac67c4bf
abdurraihan <abdurraihan898@gmail.com>
2022-05-25 02:27:43 +0600

    after implemnt jwt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors');
-
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config()
 
@@ -19,6 +19,32 @@ const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PAS}@clus
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' })
+    }
+    
+  
+    const token = authHeader.split(' ')[1];
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+  
+      req.decoded = decoded;
+      // console.log(decoded) // bar
+      next();
+    });
+  
+  }
+
+
+
+
+
 async function run() {
 
 
@@ -26,6 +52,7 @@ async function run() {
  
     await client.connect();
     const productsCollection = client.db('computer_world').collection('products');
+    const userCollection = client.db('computer_world').collection('user');
 
  
 
@@ -37,6 +64,29 @@ async function run() {
         
 
     })
+
+
+
+    // user 
+    
+    app.put('/user/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+  
+        const updateDoc = {
+          $set: user,
+        };
+  
+  
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+  
+        const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
+  
+        res.send({ result, token });
+  
+      });
 
     }finally{
 
